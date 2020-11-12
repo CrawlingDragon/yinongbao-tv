380da13a8f8a9112cceeb2f8ffbeb5455e3dfd9e
ittianzhenbai <bai1042735249@163.com>
2020-11-12 23:39:13 +0800

    专家排行更新

--- src/views/expert_paihang/expert_paihang.vue
+++ src/views/expert_paihang/expert_paihang.vue
@@ -3,28 +3,28 @@
         <div>
             这是头部
         </div>
-        <div class="swiper-container">
+        <div class="swiper-container" ref="mySwiper">
             <div class="swiper-wrapper">
                 <div class="swiper-slide" v-for="(item,index) in this.expert_info" :key="index">
                     <img :src="item.avatar" alt="">
                 </div>
             </div>
-            <div class="swiper-button-prev" @click="prev()"></div>
-            <div class="swiper-button-next" @click="next()"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
         </div>
         <div class="single_info">
                 <div class="left">
-                    <div class="mingci text1">NO.1</div>
-                    <div class="name text1">陈岩</div>
+                    <div class="mingci text1">NO.{{this.activeIndex+1}}</div>
+                    <div class="name text1">{{this.realname}}</div>
                 </div>
                 <div class="right">
                     <div class="intros">
-                        <div class="text2">擅长作物：</div>
-                        <div></div>
+                        <div class="text2">擅长作物：{{this.zuowu}}</div>
+                        <div>{{this.position}}</div>
                     </div>
                     <div class="huifu">
                         <span class="text3">总回复</span>
-                        <span class="text4"></span>
+                        <span class="text4">{{this.replycounts}}</span>
                     </div>
                 </div>
         </div>
@@ -40,7 +40,13 @@ export default {
             limit:5,
             pageShownumber:3,//控制轮播图显示几个一页
             expert_info:"",
-            cur_level:0
+            cur_level:0,
+            activeIndex:0,
+            realname:"",//专家姓名
+            paiming:"",
+            position:"",
+            zuowu:"",
+            replycounts:""
         }
     },
     created(){
@@ -48,23 +54,35 @@ export default {
     },
     mounted(){
     },
-    
     methods:{
         init(pagesize){
-        var swiper = new Swiper('.swiper-container', {
-            spaceBetween: 30,
-            loop: true,
-            slidesPerView: pagesize,
-            centeredSlides: true,
-            pagination: {
-                el: '.swiper-pagination',
-                clickable: true,
-            },
-            navigation: {
-                nextEl: '.swiper-button-next',
-                prevEl: '.swiper-button-prev',
-            },
-        })
+            var self = this
+            var mySwiper = new Swiper('.swiper-container', {
+                spaceBetween: 30,
+                // loop: true,
+                slidesPerView: pagesize,
+                centeredSlides: true,
+                pagination: {
+                    el: '.swiper-pagination',
+                    clickable: true,
+                },
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+                on:{
+                    slideChangeTransitionEnd: function () {
+                        console.log(this.activeIndex)
+                        self.activeIndex = this.activeIndex
+                     },
+                    // transitionStart(){ // 开始translate之前触发
+                    //     // 如果是第一张
+                    //     if(this.activeIndex == 0){
+                    //         this.setTranslate(0); // 因为左边会空出一张图的距离，所以设置位移为0
+                    //     }
+                    // },
+                }
+            })
         },
         getExpertinfo(appId,purview,ordertag,page,limit){
             this.$axios.fetchPost(
@@ -74,29 +92,39 @@ export default {
                 console.log(res)
                 if(res.data.code == "200"){
                     this.expert_info = res.data.data
+                    this.realname = this.expert_info[0].realname
+                    this.position = this.expert_info[0].position
+                    this.zuowu = this.expert_info[0].zuowu
+                    this.replycounts = this.expert_info[0].replycounts
                     if(res.data.data.length-0 < 5){
                         this.pageShownumber = res.data.data.length-0
                         this.$nextTick(()=>{
-                           this.init(this.pageShownumber)
+                            this.init(this.pageShownumber)
                         })
                     }else{
                         this.$nextTick(()=>{
-                           this.init(5)
+                            this.pageShownumber = 5
+                            this.init(this.pageShownumber)
                         })
                     }
                 }
             })
         },
-        prev(){
-            this.cur_level+1
-        },
-        next(){
-            this.cur_level-1
+    },
+    watch:{
+        activeIndex(newVal){
+            this.realname = this.expert_info[newVal].realname
+            this.position = this.expert_info[newVal].position
+            this.zuowu = this.expert_info[newVal].zuowu
+            this.replycounts = this.expert_info[newVal].replycounts
         }
     }
 }
 </script>
 <style lang="stylus" scoped>
+/deep/.swiper-pagination .swiper-pagination-bullet:focus {
+           outline: none;
+    }
 .paihang_content
     margin 0 40px
     font-size MicrosoftYaHei
@@ -105,7 +133,6 @@ export default {
         margin-top 218px
         width 100%
         height 350px
-        border 1px solid rgba(255, 255, 255, 0.2)
         .swiper-slide 
             text-align center
             font-size 18px
@@ -122,13 +149,30 @@ export default {
             -webkit-align-items center
             align-items center
             transition 300ms
-            transform scale(0.6)
+            transform scale(0.8)
             line-height 350px
             align-items center
             img
-                width 70%
-                height auto
+                width 250px
+                height 250px
                 border-radius 50%
+        .swiper-slide-active
+            transform scale(1)
+            img
+                width 320px
+                height 320px
+                border-radius 50%
+                border 3px solid #FF6600
+                box-shadow 0px 1px 26px #f60
+        .swiper-slide-duplicate-active
+            transform scale(1)
+            border 1px solid red
+            img
+                width 320px
+                height 320px
+                border-radius 50%
+                outline 3px solid #FF6600
+                box-shadow 0px 1px 26px #f60
     .swiper-button-next
         position fixed
         top 623px
@@ -136,6 +180,8 @@ export default {
         height 240px
         border 1px solid rgba(255, 255, 255, 0.2)
         width 80px
+        span:focus
+            outline none
     .swiper-button-prev
         position fixed
         top 623px
@@ -143,6 +189,8 @@ export default {
         height 240px
         border 1px solid rgba(255, 255, 255, 0.2)
         width 80px
+        span:focus
+            outline none
     .single_info
         text-align center
         font-size 18px
@@ -167,7 +215,10 @@ export default {
                 font-size 50px
                 color #FFFFFF
             .mingci
-                margin 48px 110px 45px 145px
+                margin-top 48px
+                text-align left
+                margin-left 130px
+                margin-bottom 20px
         .right
             width 77.3%
             .intros
